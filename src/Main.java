9b289484fac9d3c252ec0f61c095d7d77f29d905
mlstlit <Litvinov.v@mail.ru>
2024-08-30 19:48:41 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-      
+        String firstName = "Ivanё";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника  — " + fullName);
     }
+
+
 }
